7c03a65b67207e8df416fdce91fa6e95156f7488
NKR2609 <64198309+NKR2609@users.noreply.github.com>
2020-05-27 12:48:18 +0530

    Update SpeakGoodbye.js

--- MODULE-4-solution/SpeakGoodbye.js
+++ MODULE-4-solution/SpeakGoodbye.js
@@ -1,9 +1,9 @@
- (function (window){
+ (function (window) {
  var byeSpeaker ={};
 
 var speakWord = "Good Bye";
 
- byeSpeaker.speak = function(names) {
+ byeSpeaker.speak = function (names) {
   console.log(speakWord + " " + names);
 }
 	window.byeSpeaker = byeSpeaker;
