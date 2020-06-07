8906782645ed4a991c70ad47ffdf178de30ac782
NKR2609 <64198309+NKR2609@users.noreply.github.com>
2020-06-07 22:39:36 +0530

    Update script_date.js

--- DATE and TIME/script_date.js
+++ DATE and TIME/script_date.js
@@ -6,6 +6,9 @@
 					message;
 					document.getElementById('demo').innerHTML = 
 					"<b>The date and time is </b>" + "" + Date();
+				if (name === "premi") {
+					var message = "<h2>She " + "loves" + "NIRMAL!</h2>";
+				}
 			}
 			document.querySelector("button")
 			.addEventListener("click", date);
